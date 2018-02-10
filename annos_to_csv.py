d846068d271c5a8b81be3ee66f8b5cf74242fd96
Inky <jorillo311@gmail.com>
2018-02-10 12:36:59 +0500

    init

---
+++ annos_to_csv.py
@@ -0,0 +1,22 @@
+import sys
+import argparse
+from scipy.io import loadmat
+
+parser = argparse.ArgumentParser()
+parser.add_argument('phase', type=str, help="Phase: train or test", choices=['train', 'test'])
+
+args = parser.parse_args(sys.argv[1:])
+
+mat_file = 'cars_{}_annos.mat'.format(args.phase)
+csv_file = 'cars_{}_annos.csv'.format(args.phase)
+
+mat_data = loadmat(mat_file)
+annotations = mat_data['annotations'][0]
+with open(csv_file, mode='w') as csv_data:
+    for annotation in annotations:
+        x1 = annotation[0][0][0]
+        y1 = annotation[1][0][0]
+        x2 = annotation[2][0][0]
+        y2 = annotation[3][0][0]
+        fname = annotation[-1][0]
+        csv_data.write("{},{},{},{},{},{}\n".format(fname,x1,y1,x2,y2,"car"))
